6503f5fa7d8f89e687e7d6807ce1e7c46db3db2c
Alvaro Begazo <alvaro15unmsm@gmail.com>
2020-05-05 00:21:44 -0500

    cambiando la uri, ya que al comienzo puse de forma local, pero google no permite agregar una uri de localhost, pero si cuando ya esta desplegada (remota)

--- config.php
+++ config.php
@@ -16,7 +16,7 @@ $google_client->setClientId('352759389830-r7ba3haoequa6mojg4qj3k3agupcs0du.apps.
 $google_client->setClientSecret('Buw9RgVsN22tt1HdjKKfebIp');
 
 //Set the OAuth 2.0 Redirect URI
-$google_client->setRedirectUri('http://localhost:8080/demo/');
+$google_client->setRedirectUri('https://autenticacion-google.herokuapp.com/');
 
 //
 $google_client->addScope('email');

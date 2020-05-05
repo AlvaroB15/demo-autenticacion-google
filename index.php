0964be165ebdd626bc2bd4f63482c4b088a6ad3b
Alvaro Begazo <alvaro15unmsm@gmail.com>
2020-05-05 00:18:06 -0500

    agregando meta propio de google que te da

--- index.php
+++ index.php
@@ -66,6 +66,10 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>PHP Login using Google Account</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
+
+        <meta name="google-site-verification" content="9NOxi-n_px-5JH2MzkxAt-BQI6mBMaTLkTHiISmg4Fg" />
+
+        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
